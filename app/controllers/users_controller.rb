2816a41bebfafed927f33bfd888a9ccdf51b7234
Alex Gallant <agallant1201@gmail.com>
2019-12-20 08:53:35 -0700

    Add update action for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,17 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 	end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to "users_profile"
+    else
+      flash[:error] = @user.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
 	def create
 		@user = User.new(user_params)
     if @user.save
