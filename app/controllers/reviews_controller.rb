415eca279b0214859bc75d184fcdeb033a7c47c7
Alex Gallant <agallant1201@gmail.com>
2019-12-21 15:22:42 -0700

    Add helper methods to dry up controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,18 @@
 class ReviewsController<ApplicationController
 
   def new
-    @item = Item.find(params[:item_id])
+    item
   end
 
   def create
     if field_empty?
-      item = Item.find(params[:item_id])
       flash[:error] = "Please fill in all fields in order to create a review."
       redirect_to "/items/#{item.id}/reviews/new"
     else
-      @item = Item.find(params[:item_id])
-      review = @item.reviews.create(review_params)
+      review = item.reviews.create(review_params)
       if review.save
         flash[:success] = "Review successfully created"
-        redirect_to "/items/#{@item.id}"
+        redirect_to "/items/#{item.id}"
       else
         flash[:error] = "Rating must be between 1 and 5"
         render :new
@@ -23,17 +21,15 @@ class ReviewsController<ApplicationController
   end
 
   def edit
-    @review = Review.find(params[:id])
+    review
   end
 
   def update
-    review = Review.find(params[:id])
     review.update(review_params)
     redirect_to "/items/#{review.item.id}"
   end
 
   def destroy
-    review = Review.find(params[:id])
     item = review.item
     review.destroy
     redirect_to "/items/#{item.id}"
@@ -48,5 +44,15 @@ class ReviewsController<ApplicationController
   def field_empty?
     params = review_params
     params[:title].empty? || params[:content].empty? || params[:rating].empty?
+  end
+
+  private
+
+  def item
+    @item = Item.find(params[:item_id])
+  end
+
+  def review
+    @review = Review.find(params[:id])
   end
 end
