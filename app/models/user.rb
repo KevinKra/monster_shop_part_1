89380650d7531c1ccbffd1ff6dc7014fc0338fce
Ryan Allen <rallenwasd@gmail.com>
2019-12-22 14:54:53 -0700

    Refactor model level validation to fit new creation process

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates_presence_of :name, :street_address, :city, :state, :zip, :email, :password
-	attr_accessor :password_confirmation 
+	attr_accessor :password_confirmation
+
+  has_many :orders
 
 	validates :email, uniqueness: true, presence: true
 	validates_presence_of :password, require: true
