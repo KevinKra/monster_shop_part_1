5694638f3de377f9c827e5564f2acee9b50c3a65
sasloan <sebastiansloan94@gmail.com>
2019-12-21 23:15:00 -0700

    Created test for merchant dashboard address

--- spec/features/merchants/dashboard/show_spec.rb
+++ spec/features/merchants/dashboard/show_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "As a merchant user" do
   let!(:merchant) { create(:user, :merchant_user) }
+  merchant_company = Merchant.create!(name:"Meg's Bike Shop", address: "1234 Bike cr.", city:"Denver", state: "Colorado", zip: 80221)
   before {
     visit "/login"
 
@@ -14,6 +15,13 @@ RSpec.describe "As a merchant user" do
   context 'when I visit my dashboard' do
     it 'it should display content relevant to my role' do
       expect(page).to have_content("Merchant Dashboard")
+    end
+    it 'it should display my companies name and full address' do
+      expect(page).to have_content("#{merchant_company.name}")
+      expect(page).to have_content("#{merchant_company.address}")
+      expect(page).to have_content("#{merchant_company.city}")
+      expect(page).to have_content("#{merchant_company.state}")
+      expect(page).to have_content("#{merchant_company.zip}")
     end
   end
 
