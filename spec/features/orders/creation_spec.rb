c6f753068f9346afed874d9b84fbbf534facb0ee
Ryan Allen <rallenwasd@gmail.com>
2019-12-22 12:21:02 -0700

    Refactor to include test having a user logged in before checking out a cart

--- spec/features/orders/creation_spec.rb
+++ spec/features/orders/creation_spec.rb
@@ -7,6 +7,8 @@
 
 # - the date when the order was created
 RSpec.describe("Order Creation") do
+  let!(:user) { create(:user, :default_user) }
+
   describe "When I check out from my cart" do
     before(:each) do
       @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
@@ -14,6 +16,11 @@ RSpec.describe("Order Creation") do
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
       @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+
+      visit "/login"
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      click_on "Sign In"
 
       visit "/items/#{@paper.id}"
       click_on "Add To Cart"
