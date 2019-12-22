b00f098878f28ce037a7315539e9619033e8f8e6
sasloan <sebastiansloan94@gmail.com>
2019-12-21 23:45:40 -0700

    merchant user show spec. set

--- spec/features/merchants/dashboard/show_spec.rb
+++ spec/features/merchants/dashboard/show_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "As a merchant user" do
   let!(:merchant) { create(:user, :merchant_user) }
-  merchant_company = Merchant.create!(name:"Meg's Bike Shop", address: "1234 Bike cr.", city:"Denver", state: "Colorado", zip: 80221)
   before {
+		@merchant_company = Merchant.create!(name:"Meg's Bike Shop", address: "1234 Bike cr.", city:"Denver", state: "Colorado", zip: 80221)
+		@merchant_company.users << merchant
+
     visit "/login"
 
     fill_in :email, with: merchant.email
@@ -17,11 +19,11 @@ RSpec.describe "As a merchant user" do
       expect(page).to have_content("Merchant Dashboard")
     end
     it 'it should display my companies name and full address' do
-      expect(page).to have_content("#{merchant_company.name}")
-      expect(page).to have_content("#{merchant_company.address}")
-      expect(page).to have_content("#{merchant_company.city}")
-      expect(page).to have_content("#{merchant_company.state}")
-      expect(page).to have_content("#{merchant_company.zip}")
+      expect(page).to have_content("#{@merchant_company.name}")
+      expect(page).to have_content("#{@merchant_company.address}")
+      expect(page).to have_content("#{@merchant_company.city}")
+      expect(page).to have_content("#{@merchant_company.state}")
+      expect(page).to have_content("#{@merchant_company.zip}")
     end
   end
 
