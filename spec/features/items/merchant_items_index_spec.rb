4aa7b3ada7f1e78a5d3b306b38ad8d4ef7a3b034
Ryan Allen <rallenwasd@gmail.com>
2020-01-06 16:32:05 -0700

    Refactor testing for deleted items to merchant_items_index_spec
    Co-Authored-By: Sebastian Sloan <51456013+sasloan@users.noreply.github.com>

--- spec/features/items/merchant_items_index_spec.rb
+++ spec/features/items/merchant_items_index_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.describe "Merchant Items Index Page" do
   describe "When I visit the merchant items page" do
     before(:each) do
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      @tire = @meg.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @chain = @meg.items.create!(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      @shifter = @meg.items.create!(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+
+      order = Order.create!(name: "Ryan's Order", address: "123", city: "pekin", state: "illinois", zip: "61554")
+      # order.items << [@shifter]
+      ItemOrder.create!(order_id: order.id, item_id: @shifter.id, price: @shifter.price, quantity: 1)
 
       visit "merchants/#{@meg.id}/items"
     end
@@ -21,6 +25,7 @@ RSpec.describe "Merchant Items Index Page" do
         expect(page).to have_content("Inventory: #{@tire.inventory}")
         expect(page).to have_link("Deactivate Item")
         expect(page).not_to have_link("Activate Item")
+        expect(page).to have_link("Delete Item")
       end
 
       within "#item-#{@chain.id}" do
@@ -32,7 +37,7 @@ RSpec.describe "Merchant Items Index Page" do
         expect(page).to have_content("Inventory: #{@chain.inventory}")
         expect(page).to have_link("Deactivate Item")
         expect(page).not_to have_link("Activate Item")
-
+        expect(page).to have_link("Delete Item")
       end
 
       within "#item-#{@shifter.id}" do
@@ -44,10 +49,11 @@ RSpec.describe "Merchant Items Index Page" do
         expect(page).to have_content("Inventory: #{@shifter.inventory}")
         expect(page).to have_link("Activate Item")
         expect(page).not_to have_link("Deactivate Item")
+        expect(page).not_to have_link("Delete Item")
       end
     end
 
-    it 'I click Deactive Item and item is then deactivated' do
+    it 'I click Deactivate Item and item is then deactivated' do
       within "#item-#{@chain.id}" do
         click_link 'Deactivate Item'
       end
@@ -69,7 +75,7 @@ RSpec.describe "Merchant Items Index Page" do
       end
     end
 
-    it 'I click Deactive Item and item is then deactivated' do
+    it 'I click activate Item and item is then deactivated' do
       within "#item-#{@shifter.id}" do
         click_link 'Activate Item'
       end
@@ -89,6 +95,44 @@ RSpec.describe "Merchant Items Index Page" do
         expect(page).to have_content("Active")
         expect(page).to have_link("Deactivate Item")
         expect(page).not_to have_link("Activate Item")
+      end
+    end
+
+    it "I click delete and the item is deleted" do
+      within "#item-#{@tire.id}" do
+        click_link("Delete Item")
+      end
+
+      expect(current_path).to eq("/merchants/#{@meg.id}/items")
+
+      within "#main-flash" do
+        expect(page).to have_content("Item #{@tire.name} has been deleted.")
+      end
+
+      expect(page).not_to have_css("#item-#{@tire.id}")
+
+      within "#item-#{@shifter.id}" do
+        expect(page).to have_content(@shifter.name)
+        expect(page).to have_content("Price: $#{@shifter.price}")
+        expect(page).to have_css("img[src*='#{@shifter.image}']")
+        expect(page).to have_content("Inactive")
+        expect(page).not_to have_content(@shifter.description)
+        expect(page).to have_content("Inventory: #{@shifter.inventory}")
+        expect(page).to have_link("Activate Item")
+        expect(page).not_to have_link("Deactivate Item")
+        expect(page).not_to have_link("Delete Item")
+      end
+
+      within "#item-#{@chain.id}" do
+        expect(page).to have_content(@chain.name)
+        expect(page).to have_content("Price: $#{@chain.price}")
+        expect(page).to have_css("img[src*='#{@chain.image}']")
+        expect(page).to have_content("Active")
+        expect(page).not_to have_content(@chain.description)
+        expect(page).to have_content("Inventory: #{@chain.inventory}")
+        expect(page).to have_link("Deactivate Item")
+        expect(page).not_to have_link("Activate Item")
+        expect(page).to have_link("Delete Item")
       end
     end
   end
