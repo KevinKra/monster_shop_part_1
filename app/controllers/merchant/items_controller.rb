7dc12d8be0706e94f07bedf1babe7ef9f388cb24
Ryan Allen <rallenwasd@gmail.com>
2020-01-06 22:20:04 -0700

    Add before action for merchant only access and update action

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,5 +1,20 @@
 class Merchant::ItemsController < ApplicationController
+  before_action :require_merchant_access
+
   def index
-    @items = current_user.merchant.items
+    @items = Item.where(merchant_id: current_user.merchant.id)
+  end
+
+  def update
+    @item = Item.find(params[:item_id])
+    @item.update(active?: false)
+    flash[:notice] = "Item '#{@item.name}' is no longer for sale."
+    redirect_to '/merchant/items'
+  end
+
+  private
+
+  def require_merchant_access
+    render file: "/public/404" unless current_merchant?
   end
 end
