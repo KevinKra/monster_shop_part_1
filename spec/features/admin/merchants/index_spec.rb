e354313de83c4e457d17fd0262dddc3f5e5a8c16
Kevin Krato <kratok08@gmail.com>
2020-01-08 16:18:35 -0700

    Adjust tests to handle ambigious match error

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -2,45 +2,49 @@ require "rails_helper"
 
 RSpec.describe "admin/merchants#index" do
   let!(:admin) { create(:user, :admin_user) }
-  let!(:merchant) { create_list(:merchant, 5) }
+  let!(:merchant) { create_list(:merchant, 3) }
 
   before {
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    @merchant_1, @merchant_2 = create_list(:merchant, 2)
+    @merchant_1.disabled = false
+    visit "/admin/merchants"
   }
 
   context "as an admin visiting the merchants page" do
     it "admin should see a 'disable' button next to any merchants who are not yet disabled" do
-      visit "/admin/merchants" 
-
       expect(current_path).to eq("/admin/merchants")
-      expect(page).to have_content("Disable")
+      within("#merchant-#{@merchant_1.id}") do
+        expect(page).to have_content("Disable")
+      end
     end
 
     it "when the admin clicks the 'disable' button, they are returned to the admin's merchant index page" do
-      visit "/admin/merchants" 
+      within("#merchant-#{@merchant_1.id}") do
+        click_on "Disable"
+      end
 
-      click_on "Disable"
-      
       expect(current_path).to eq("/admin/merchants")
-      expect(page).to have_content("Enable")
+
+      within("#merchant-#{@merchant_1.id}") do
+        expect(page).to have_content("Enable")
+      end
     end
 
-    # it "upon visiting the merchant index page, the merchant's account is now disabled" do
-
-    # end
-
     it "the admin sees a flash message that the merchant's account is disabled/enabled" do
-      visit "/admin/merchants" 
-
-      click_on "Disable"
+      within("#merchant-#{@merchant_1.id}") do
+        click_on "Disable"
+      end
       
       expect(current_path).to eq("/admin/merchants")
-      expect(page).to have_content("#{merchant.name} is now Disabled")
+      expect(page).to have_content("#{@merchant_1.name} is now Disabled")
 
-      click_on "Enable"
+      within("#merchant-#{@merchant_1.id}") do
+        click_on "Enable"
+      end
 
       expect(current_path).to eq("/admin/merchants")
-      expect(page).to have_content("#{merchant.name} is now Enabled")
+      expect(page).to have_content("#{@merchant_1.name} is now Enabled")
     end
   end
 end
