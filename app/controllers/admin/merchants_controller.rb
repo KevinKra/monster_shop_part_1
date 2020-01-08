0ca2d4dc13d5434557e2843b6117376328b9cff3
Kevin Krato <kratok08@gmail.com>
2020-01-08 10:34:17 -0700

    Add and define update method for admin merchants page

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,4 +1,18 @@
 class Admin::MerchantsController < Admin::BaseController
   def index
+    @merchants = Merchant.all
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    if merchant.disabled?
+      merchant.update!(disabled: false)
+      redirect_to "/admin/merchants"
+      flash[:success] = "#{merchant.name} is now Enabled." 
+    else
+      merchant.update!(disabled: true)
+      redirect_to "/admin/merchants"
+      flash[:success] = "#{merchant.name} is now Disabled."
+    end
   end
 end
