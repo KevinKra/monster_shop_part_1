c900e0090dbd8dc82a4aaf498fce32260e811026
Kevin Krato <kratok08@gmail.com>
2020-01-13 16:34:13 -0700

    Create new and create actions for Merchant Coupons controller

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -2,4 +2,28 @@ class Merchant::CouponsController < Merchant::BaseController
   def index
     @coupons = Coupon.all
   end
+
+  def new
+  end
+
+  def create
+    coupon = current_user.merchant.coupons.new(coupon_params)
+    if coupon.save
+      flash[:success] = "Coupon successfully added"
+      redirect_to "/merchant/coupons"
+    else
+      flash[:error] = coupon.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+    def coupon_params 
+      params.permit(
+        :name,
+        :code,
+        :discount,
+        :active
+      )
+    end
 end
