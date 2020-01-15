48179d4b81cfe10934b6a99721ba727fdf8f5bbf
Kevin Krato <kratok08@gmail.com>
2020-01-14 19:46:23 -0700

    Test and create discount handling mechanics

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,6 @@ class OrdersController < ApplicationController
 
   def index
     @orders = current_user.orders
-    # current_user.orders
   end
 
   def update
@@ -25,15 +24,18 @@ class OrdersController < ApplicationController
   end
 
   def create
-    order = current_user.orders.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+    # i dont know why this is here, but removing it breaks a lot of tests i had nothing to do with. If i have time to fix it, i will.
+    order = current_user.orders.create(name: '???', address: '123  Ave', city: 'Hershey', state: 'PA', zip: 17033)
+    coupon_id = session[:coupon_id]
     cart.items.each do |item,quantity|
       order.item_orders.create({
         item: item,
         quantity: quantity,
-        price: item.price
+        price: item.coupon_check(coupon_id)
         })
     end
     session.delete(:cart)
+    session.delete(:coupon_id)
     flash[:notice] = "Your order has been created."
     redirect_to '/profile/orders'
   end
